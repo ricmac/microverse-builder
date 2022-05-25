15277e34aff0d1974c4bcdb0c98c4ed90cdafcf1
ricmac <ricmac@users.noreply.github.com>
2022-05-25 15:22:28 +0100

    setup Croquet
    setup

---
+++ apiKey.js
@@ -0,0 +1,5 @@
+const apiKey = "1y2xOpf3hDMqDNOt9scf7PxNL5qPNetylJ8jioo0A";
+const appId = "type your own appId such as com.example.ricmac.mymicroverse";
+export default {apiKey, appId};
+
+// you may export other Croquet session parameters to override default values.
