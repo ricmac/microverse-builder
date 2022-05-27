bcfeafff5f03049626fa3474b334b4dc19eef602
ricmac <ricmac@users.noreply.github.com>
2022-05-27 16:16:07 +0100

    Update apiKey.js
    Fixed url

--- apiKey.js
+++ apiKey.js
@@ -1,5 +1,5 @@
 const apiKey = "1nfIGq7bzdxIosErfmuvOgsvFxOl5i4jxCEMftNmc";
-const appId = "type your own appId such as com.example.ricmac.mymicroverse";
+const appId = "com.example.ricmac.mymicroverse";
 export default {apiKey, appId};
 
 // you may export other Croquet session parameters to override default values.
